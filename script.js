bdb93770bf090536390228b09696547b0e1db3a9
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2025-07-01 17:42:47 +0530

    Update script.js
    add js code for image slider

--- script.js
+++ script.js
@@ -156,6 +156,25 @@ document.getElementById("githubLinkBtn5").addEventListener("click", function() {
     window.open(githubUrl, "_blank");
 });
 
+   let currentIndex = 0;
+  const slider = document.getElementById("slider");
+  const slides = slider.children;
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+  }
+
    
 
 
